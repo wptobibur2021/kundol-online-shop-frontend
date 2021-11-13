e71751b9d61d9f8c1b15a0758c21a50485569ccf
Md Tobibur Rohman <71486299+wptobibur2021@users.noreply.github.com>
2021-11-13 18:55:17 +0600

    Some

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -1,28 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAO3OWfA72eh-Xm81WY_tJTnwc4Mr_CTU8",
-    authDomain: "kundol-2021.firebaseapp.com",
-    projectId: "kundol-2021",
-    storageBucket: "kundol-2021.appspot.com",
-    messagingSenderId: "484953129094",
-    appId: "1:484953129094:web:1662e2e13a1e4e26075107"
-};
-export default firebaseConfig
-
-/*
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-*/
+};
+export default firebaseConfig
 
-/*
-    apiKey: "AIzaSyAdWhia7K_Scevu9wq4ObATAqbRgdTnlnM",
-    authDomain: "doctorsportal-2021.firebaseapp.com",
-    projectId: "doctorsportal-2021",
-    storageBucket: "doctorsportal-2021.appspot.com",
-    messagingSenderId: "85696456331",
-    appId: "1:85696456331:web:91cf6f1941b53937b8168e"
-*/
